650c1ff165639a906f34f6d33c81027369078259
Akashkale96 <kaleakash83@gmail.com>
2021-06-22 22:36:41 +0530

    Headless

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,12 +11,24 @@ public class Base
     public static void OpebTheBrowser()
     {
 
+//        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().deleteAllCookies();
+//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//        driver.get("https://mail.google.com/");
+
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
+       driver.manage().window().maximize();
+       driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://mail.google.com/");
+
+
     }
 
 }
