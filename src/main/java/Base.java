b37a258516d6531532907b3e03af3cb9e0d81a81
Akashkale96 <kaleakash83@gmail.com>
2021-06-28 11:57:37 +0530

    modify

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -18,18 +18,6 @@ public class Base
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://mail.google.com/");
 
-
-//        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-//        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.manage().deleteAllCookies();
-//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-//        driver.get("https://mail.google.com/");
-
-
     }
 
 }
