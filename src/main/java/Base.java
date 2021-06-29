c4f9b701cbcd56528b7ef65c732c78744f07b868
Akashkale96 <kaleakash83@gmail.com>
2021-06-29 15:32:36 +0530

    modify1

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -11,23 +11,23 @@ public class Base
     public static void OpebTheBrowser()
     {
 
-//        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.manage().deleteAllCookies();
-//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-//        driver.get("https://mail.google.com/");
-//
-
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://mail.google.com/");
+
+
+//        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().deleteAllCookies();
+//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//        driver.get("https://mail.google.com/");
 
 
 
